d6cc2d44e2d7f1968bdcdfcc5dfb80aadd798911
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-09-15 14:26:26 +0300

    completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,13 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const singleLink = links.map((link) => {
+   return  <a key ={link} href={"#" + link}>{link}</a>
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+  {singleLink}
+  </nav>;
 }
 
 export default NavBar;
